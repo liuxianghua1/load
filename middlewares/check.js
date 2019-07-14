c909dfa54db0bd86e758fab2f994d5fb472375d3
救罚 <52877028+liuxianghua1@users.noreply.github.com>
2019-07-14 18:38:00 +0800

    Update check.js

--- middlewares/check.js
+++ middlewares/check.js
@@ -9,7 +9,7 @@ module.exports = {
   
     checkNotLogin: function checkNotLogin (req, res, next) {
       if (req.session.admin) {
-        req.flash('error', '已登录.')
+        req.flash('error', '已登录')
         return res.redirect('back')// 返回之前的页面
       }
       next()
